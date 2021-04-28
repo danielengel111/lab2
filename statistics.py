ff88ceeccfc6bcc4a49e031b20bd15f1aa409204
maayan <maayanmagal@gmail.com>
2021-04-28 14:48:39 +0300

    comment

--- statistics.py
+++ statistics.py
@@ -2,6 +2,11 @@ import math
 
 
 def median(list_of_values):
+    """
+    returns the median of a list of values
+    :param list_of_values: the values to calculate the median
+    :return: the median
+    """
     sorted_list = sorted(list_of_values)
     center_index = int(len(list_of_values)/2) # round to int required because division always produces float
 
@@ -15,16 +20,32 @@ def median(list_of_values):
 
 
 def mean(list_of_values):
+    """
+    returns the mean of a list of values
+    :param list_of_values: the values to calculate the mean
+    :return: the mean
+    """
     return sum(list_of_values)/len(list_of_values)
 
 
 def variance(list_of_values):
+    """
+    returns the variance of a list of values
+    :param list_of_values: the values to calculate the variance
+    :return: the variance
+    """
     average = mean(list_of_values)
     squared_sum = sum([(x - average)**2 for x in list_of_values])
     return squared_sum/(len(list_of_values)-1)
 
 
 def covariance(first_list_of_values, second_list_of_values):
+    """
+    calculates the covariance of two lists of values
+    :param first_list_of_values: the first list of values
+    :param second_list_of_values: the second list of values
+    :return: the covariance
+    """
     result = 0
     # Place your code here
     ####################### start
@@ -37,6 +58,12 @@ def covariance(first_list_of_values, second_list_of_values):
 
 
 def correlation(first_list_of_values, second_list_of_values):
+    """
+    calculates the correlation of two lists of values
+    :param first_list_of_values: the first list of values
+    :param second_list_of_values: the second list of values
+    :return: the correlation
+    """
     result = 0
     # Place your code here
     ####################### start
